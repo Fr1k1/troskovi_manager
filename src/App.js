b1b9ff6e2132231811e6519c63035b3b17435293
Fr1k1 <martin.friscic6@gmail.com>
2023-02-27 20:31:33 +0100

    Added login, logout

--- src/App.js
+++ src/App.js
@@ -1,95 +1,67 @@
-import "./App.scss";
-import { db } from "./firebase";
-import { uid } from "uid";
-import { set, ref, onValue, remove, update } from "firebase/database";
-import { useState, useEffect } from "react";
+
+import { useEffect, useState } from "react";
+import { signInWithEmailAndPassword, onAuthStateChanged, signOut, } from "firebase/auth";
+import { auth } from "./firebase";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-  const [isEdit, setIsEdit] = useState(false);
-  const [tempUuid, setTempUuid] = useState("");
 
-  const handleTodoChange = (e) => {
-    setTodo(e.target.value);
-  };
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [user, setUser] = useState({});
 
-  //read
+
   useEffect(() => {
-    onValue(ref(db), (snapshot) => {
-      setTodos([]);
-      const data = snapshot.val();
-      if (data !== null) {
-        Object.values(data).map((todo) => {
-          setTodos((oldArray) => [...oldArray, todo]);
-        });
-      }
-    });
-  }, []);
-
-  //write
-  const writeToDatabase = () => {
-    const uuid = uid();
-    set(ref(db, `/${uuid}`), {
-      todo,
-      uuid,
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
     });
 
-    setTodo("");
+  }, [])
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+
+  const logout = async () => {
+    await signOut(auth);
   };
 
-  //update
-  const handleUpdate = (todo) => {
-    setIsEdit(true);
-    setTempUuid(todo.uuid);
-    setTodo(todo.todo);
-  };
-
-  const handleSubmitChange = () => {
-    update(ref(db, `/${tempUuid}`), {
-      todo,
-      uuid: tempUuid,
-    });
-
-    setTodo("");
-    setIsEdit(false);
-  };
-
-  //delete
-  const handleDelete = (todo) => {
-    remove(ref(db, `/${todo.uuid}`));
-  };
 
   return (
-    <div className="App">
-      <input type="text" value={todo} onChange={handleTodoChange} />
-      {isEdit ? (
-        <>
-          <button onClick={handleSubmitChange}>Submit Change</button>
-          <button
-            onClick={() => {
-              setIsEdit(false);
-              setTodo("");
-            }}
-          >
-            X
-          </button>
-        </>
-      ) : (
-        <button onClick={writeToDatabase}>submit</button>
-      )}
-      {todos.map((todo) => (
-        <>
-          <h1>{todo.todo}</h1>
-          <button onClick={() => handleUpdate(todo)}>update</button>
-          <button onClick={() => handleDelete(todo)}>delete</button>
-        </>
-      ))}
+    <div>
+      <h3> Login </h3>
+      <input
+        placeholder="Email..."
+        onChange={(event) => {
+          setLoginEmail(event.target.value);
+        }}
+      />
+      <input
+        placeholder="Password..."
+        onChange={(event) => {
+          setLoginPassword(event.target.value);
+        }}
+      />
+
+      <button onClick={login}> Login</button>
+
+      <h4> User Logged In: </h4>
+      {user?.email}
+
+      <button onClick={logout}> Sign Out </button>
     </div>
+
+
   );
 }
 
 export default App;
-
-// npm install firebase
-// npm install uid
